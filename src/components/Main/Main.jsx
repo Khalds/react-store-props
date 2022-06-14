654f2c8e0c6591dfd59db24f8f00679a309b0798
khald <abv.khallid@mail.ru>
2022-06-14 11:08:04 +0300

    added array

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -15,40 +15,67 @@ function Main() {
 
 const product = [
   {
-    image:
-      "https://st.depositphotos.com/3235977/4269/i/600/depositphotos_42699295-stock-photo-macbook-air.jpg",
-    price: 97000,
-    name: "macbook",
+    id: 1,
+    name: "MacBook",
+    price: 100000,
+    left: 31,
+    image: "http://intocode.ru/d/react-project-1/images/1.jpg",
   },
   {
-    image:
-      "https://st.depositphotos.com/3235977/4269/i/600/depositphotos_42699295-stock-photo-macbook-air.jpg",
-    price: 97000,
-    name: "macbook",
+    id: 2,
+    name: "Galaxy",
+    price: 35999,
+    left: 11,
+    image: "http://intocode.ru/d/react-project-1/images/2.jpg",
   },
   {
-    image:
-      "https://st.depositphotos.com/3235977/4269/i/600/depositphotos_42699295-stock-photo-macbook-air.jpg",
-    price: 97000,
-    name: "macbook",
+    id: 3,
+    name: "Скутер",
+    price: 65500,
+    left: 0,
+    image: "http://intocode.ru/d/react-project-1/images/3.jpg",
   },
   {
-    image:
-      "https://st.depositphotos.com/3235977/4269/i/600/depositphotos_42699295-stock-photo-macbook-air.jpg",
-    price: 97000,
-    name: "macbook",
+    id: 4,
+    name: "Монитор Samsung",
+    price: 12000,
+    left: 7,
+    image: "http://intocode.ru/d/react-project-1/images/4.jpg",
   },
   {
-    image:
-      "https://st.depositphotos.com/3235977/4269/i/600/depositphotos_42699295-stock-photo-macbook-air.jpg",
-    price: 97000,
-    name: "macbook",
+    id: 5,
+    name: "Респераторная маска",
+    price: 500,
+    left: 24,
+    image: "http://intocode.ru/d/react-project-1/images/5.jpg",
   },
   {
-    image:
-      "https://st.depositphotos.com/3235977/4269/i/600/depositphotos_42699295-stock-photo-macbook-air.jpg",
-    price: 97000,
-    name: "macbook",
+    id: 6,
+    name: "Стиральная машина",
+    price: 100000,
+    left: 0,
+    image: "http://intocode.ru/d/react-project-1/images/6.jpg",
+  },
+  {
+    id: 7,
+    name: "Белый холодильник",
+    price: 43100,
+    left: 18,
+    image: "http://intocode.ru/d/react-project-1/images/7.jpg",
+  },
+  {
+    id: 8,
+    name: "Колонка",
+    price: 3000,
+    left: 1,
+    image: "http://intocode.ru/d/react-project-1/images/8.jpg",
+  },
+  {
+    id: 9,
+    name: "Наушники",
+    price: 1500,
+    left: 5,
+    image: "http://intocode.ru/d/react-project-1/images/9.jpg",
   },
 ]
 
